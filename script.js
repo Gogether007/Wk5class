3d4a5a48772b65703b675ba7d8fa77f81791a458
Gogether007 <ortizchristina881@gmail.com>
2025-01-05 18:40:46 +0000

    wk5 part 2

--- script.js
+++ script.js
@@ -4,8 +4,27 @@ console.log( "temp")
 let c=prompt("enter temo in c")
 
 let f=Number(c)*9/5+32
+// 
 
 alert(f)
+
+// age verification
+let age=prompt("what is you age?")
+
+ age=Number(age)
+alert(age)
+if(age<18){
+    alert("user is a minor")
+} else if (age<65) {
+    alert("Congradulations grown up")
+}
+else { 
+    alert("senior")
+
+}
+
+
+
 
 
 
